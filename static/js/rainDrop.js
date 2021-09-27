7c1bcc2cb71c802179580800d3e7e006d60ea2e6
Jerry3T <jeremygarciabwhs@gmail.com>
2021-09-27 13:59:45 -0400

    glitched effect

--- static/js/rainDrop.js
+++ static/js/rainDrop.js
@@ -9,51 +9,52 @@ var rain = function() {
 
     while (increment < 100) {
 
-    var randoHundo = Math.floor(Math.random() * (98 - 1 + 1) + 1);
+        var randoHundo = Math.floor(Math.random() * (98 - 1 + 1) + 1);
 
-    var randoFiver = Math.floor(Math.random() * (5 - 2 + 1) + 2);
+        var randoFiver = Math.floor(Math.random() * (5 - 2 + 1) + 2);
 
-    increment += randoFiver;
+        increment += randoFiver;
 
-    drops +=
-        '<div class="drop" style="left: ' +
-        increment +
-        "%; bottom: " +
-        (randoFiver + randoFiver - 1 + 90) +
-        "%; animation-delay: 0." +
-        randoHundo +
-        "s; animation-duration: 0.5" +
-        randoHundo +
-        's;"><div class="stem" style="animation-delay: 0.' +
-        randoHundo +
-        "s; animation-duration: 0.5" +
-        randoHundo +
-        's;"></div><div class="splat" style="animation-delay: 0.' +
-        randoHundo +
-        "s; animation-duration: 0.5" +
-        randoHundo +
-        's;"></div></div>';
-    backDrops +=
-        '<div class="drop" style="right: ' +
-        increment +
-        "%; bottom: " +
-        (randoFiver + randoFiver - 1 + 90) +
-        "%; animation-delay: 0." +
-        randoHundo +
-        "s; animation-duration: 0.5" +
-        randoHundo +
-        's;"><div class="stem" style="animation-delay: 0.' +
-        randoHundo +
-        "s; animation-duration: 0.5" +
-        randoHundo +
-        's;"></div><div class="splat" style="animation-delay: 0.' +
-        randoHundo +
-        "s; animation-duration: 0.5" +
-        randoHundo +
-        's;"></div></div>';
+        drops +=
+            '<div class="drop" style="left: ' +
+            increment +
+            "%; bottom: " +
+            (randoFiver + randoFiver - 1 + 90) +
+            "%; animation-delay: 0." +
+            randoHundo +
+            "s; animation-duration: 0.5" +
+            randoHundo +
+            's;"><div class="stem" style="animation-delay: 0.' +
+            randoHundo +
+            "s; animation-duration: 0.5" +
+            randoHundo +
+            's;"></div><div class="splat" style="animation-delay: 0.' +
+            randoHundo +
+            "s; animation-duration: 0.5" +
+            randoHundo +
+            's;"></div></div>';
+        backDrops +=
+            '<div class="drop" style="right: ' +
+            increment +
+            "%; bottom: " +
+            (randoFiver + randoFiver - 1 + 90) +
+            "%; animation-delay: 0." +
+            randoHundo +
+            "s; animation-duration: 0.5" +
+            randoHundo +
+            's;"><div class="stem" style="animation-delay: 0.' +
+            randoHundo +
+            "s; animation-duration: 0.5" +
+            randoHundo +
+            's;"></div><div class="splat" style="animation-delay: 0.' +
+            randoHundo +
+            "s; animation-duration: 0.5" +
+            randoHundo +
+            's;"></div></div>';
+
     }
 
-    document.getElementById('rain').innerHTML = drops;
+    document.getElementById("rain").innerHTML = drops;
     document.getElementById("rain-back-row").innerHTML = backDrops;
 
 }
---
+++ static/js/skewer.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+
+    document.getElementById("glitch").style.transform =
+      "translateX(-2.7em) translateY(-0.65em) skew(0.06turn)"
+
+}, 500)
